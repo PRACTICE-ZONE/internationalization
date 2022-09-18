512a3b1d477be71be1ae781949edf1dda8b9ede0
IBUNHABIBU <ibunhabib@gmail.com>
2022-09-18 22:28:29 +0300

    Add checkout session

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,14 +1,14 @@
 class CheckoutController < ApplicationController
   def create
-    session = Stripe::Checkout::Session.create({
+    @session = Stripe::Checkout::Session.create({
       payment_method_types: ['card'],
       line_items: [{
-        price: 'price_1J3c3qK9Xy6Pfj4J4M4g4o2e',
+        price: 'price_1LjS1UGwlInU1VC0KX5cyxil',
         quantity: 1,
       }],
-      mode: 'payment',
-      success_url: checkout_success_url,
-      cancel_url: checkout_cancel_url,
+      mode: 'subscription',
+      success_url: posts_url,
+      cancel_url: pricing_url,
     })
     render json: { id: session.id }
   end
