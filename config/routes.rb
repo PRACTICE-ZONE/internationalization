2fd0da74a52d0d054a6091e6f68c8015e045ecd7
IBUNHABIBU <ibunhabib@gmail.com>
2022-09-18 20:44:28 +0300

    Modify the nav

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     resources :feedbacks
     root 'static_pages#index'
+    get 'pricing', to: 'static_pages#pricing'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
